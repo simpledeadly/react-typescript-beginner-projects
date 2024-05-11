6777ca0d4b50193885714b06955212167952e3dc
simpledeadly <markmarkov94186@gmail.com>
2024-05-11 13:36:56 +0700

    finish modal with typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,48 @@
-import { Dispatch, FC, useState } from 'react'
+import { Squircle } from '@squircle-js/react'
+import { Dispatch, FC, ReactNode, useState } from 'react'
 import './index.scss'
 
+interface ModalProps { modal: boolean, setModal: Dispatch<boolean>, children: ReactNode }
+
+const Modal: FC<ModalProps> = ({ modal, setModal, children }: ModalProps): JSX.Element => {
+  return (
+    <div className={`overlay animated ${modal && 'show'}`}>
+      <Squircle
+        cornerRadius={15}
+        cornerSmoothing={1}
+        className="modal"
+      >
+        { children }
+        <Squircle
+          cornerRadius={10}
+          cornerSmoothing={1}
+          className="close-modal-btn"
+          onClick={(): void => setModal(false)}
+        >
+          Закрыть
+        </Squircle>
+      </Squircle>
+    </div>
+  )
+}
+
 export const App: FC = (): JSX.Element => {
-  const [counter, setCounter]: [number, Dispatch<number>] = useState<number>(0)
+  const [modal, setModal]: [boolean, Dispatch<boolean>] = useState<boolean>(false)
 
   return (
-    <div className='App'>
-      <div>
-        <h2>Счётчик:</h2>
-        <h1>{counter}</h1>
-        <button onClick={(): void => setCounter(counter - 1)} className='minus'>Минус -</button>
-        <button onClick={(): void => setCounter(counter + 1)} className='plus'>Плюс +</button>
-      </div>
+    <div className="App">
+      <Squircle
+        cornerRadius={10}
+        cornerSmoothing={1}
+        className="open-modal-btn"
+        onClick={(): void => setModal(true)}
+      >
+        Открыть модалку
+      </Squircle>
+      <Modal modal={modal} setModal={setModal}>
+        <h1>Modal!</h1>
+        <p>Yes, it works.</p>
+      </Modal>
     </div>
   )
 }
