0f8827a042f0fe533910d4cb8eba5b44f10a09d8
simpledeadly <markmarkov94186@gmail.com>
2024-05-26 12:56:59 +0700

    init curr-conver

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,10 @@
-import { FC, useEffect, useState } from 'react'
-import { Success } from './components/Success'
-import { Users } from './components/Users/Users'
-import { OnClickInvite, User } from './types'
+import { Block } from './Block'
 
-// список пользователей: https://reqres.in/api/users
-
-export const App: FC = () => {
-  const [loading, setLoading] = useState<boolean>(false)
-  const [users, setUsers] = useState<User[]>([])
-  const [invites, setInvites] = useState<number[]>([])
-	const [success, setSuccess] = useState<boolean>(false)
-
-  useEffect(() => {
-    setLoading(true)
-    fetch('https://reqres.in/api/users')
-      .then(res => res.json())
-      .then(res => setUsers(res.data))
-    setLoading(false)
-  }, [])
-
-  const onClickInvite: OnClickInvite = (id) => {
-    if (invites.includes(id)) {
-      setInvites(prev => prev.filter(_id => _id !== id))
-    } else {
-      setInvites(prev => [...prev, id])
-    }
-  }
-
+export const App = () => {
   return (
     <div className="App">
-      {!success ? (
-          <Users loading={loading} items={users} invites={invites} success={success} setSuccess={setSuccess} onClickInvite={onClickInvite} />
-        ) : (
-          <Success count={invites.length} setSuccess={setSuccess} />
-        )
-      }
+      <Block value={0} currency="RUB" onChangeCurrency={cur => console.log(cur)} />
+      <Block value={0} currency="USD" />
     </div>
-  )
+  );
 }
